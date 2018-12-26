9ddf0d082cb3d139f226984d4a02e36861ccb864
Juan Gabriel Velásquez Ortiz <jgvelort1611@gmail.com>
2018-12-26 14:57:50 -0500

    Optimizar busqueda
    Se optimiz el Service de Spotify para llamar de forma mas cómoda los services de la API

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+import { map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,14 +10,27 @@ export class SpotifyService {
 
   constructor(private http: HttpClient) { }
 
-  getNewReleases() {
+  getQuery( query: string ) {
+    const url = `https://api.spotify.com/v1/${ query }`;
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQDkCnlX-v8lyiN8SepQfU7Ud5vMEABHMRW8klHhJ8Ge2-nHZrwhWHUaABKCZzeJniWgXFA9-cN7CbSVAcQ'
+      'Authorization': 'Bearer BQDzAHvACo7HTlbop2Wg0fVe7AksDJH1jYiH8WnPbhdkI6DbUYV1typU2IOTcHTe9yHmCKFKAnh2Zu7Aaa4'
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });
+    return this.http.get(url, {headers});
+  }
 
+  getNewReleases() {
+
+    return this.getQuery('browse/new-releases?limit=20')
+      .pipe( map( data => {
+        return data['albums'].items;
+      }));
+  }
+
+  getArtista ( termino: string ) {
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`) //Ese parametro en backticks se llama template-string
+    .pipe( map( data => data['artists'].items)); // Cuando en una funcion de flecha es un return de una sola linea, se puede escribir asi
   }
 
 }
